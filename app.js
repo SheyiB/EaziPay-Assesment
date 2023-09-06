2d1590bd098ed787bf88c9d3efc304ce64fe9e4e
Seyi-Banjo <elijahbanjo@gmail.com>
2023-09-07 00:33:33 +0100

    Folder Restructure

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ const schema = require('./schema/schema')
  
  
 const app = express();
+
+
+const mongoose = require('mongoose');
+ 
+mongoose.connect('mongodb://127.0.0.1:27017/EaziPay')
+ 
+mongoose.connection.once('open', () => {
+ 
+   console.log('connected to database');
+ 
+});
  
 
  
